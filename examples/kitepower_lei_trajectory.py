1a3efcec0fceb012377855e7013be8311860ba00
Maher Brahim <maherbrahim.127@gmail.com>
2025-05-23 14:40:07 +0200

    small changes

--- examples/kitepower_lei_trajectory.py
+++ examples/kitepower_lei_trajectory.py
@@ -30,7 +30,7 @@ options['user_options.system_model.kite_dof'] = 3
 # indicate desired operation mode
 options['user_options.trajectory.type'] = 'power_cycle'
 options['user_options.trajectory.system_type'] = 'lift_mode'
-windings = 4
+windings = 1
 options['user_options.trajectory.lift_mode.windings'] = windings
 
 # indicate desired environment
@@ -239,7 +239,7 @@ wind = plot_dict['outputs']['aerodynamics']['u_infty1']
 apparent_wind = plot_dict['outputs']['aerodynamics']['vec_u1']
 kite_vel = plot_dict['x']['dq10']
 true_apparent_wind = plot_dict['outputs']['aerodynamics']['true_vec_u1']
-# animate_3d_flight(kite_positions, [e_x, wind, kite_vel, apparent_wind, true_apparent_wind], force_labels=["e_x", "wind", "kite_vel", "apparent wind", "true_apparent_wind"])
+animate_3d_flight(kite_positions, [e_x, wind, kite_vel, apparent_wind, true_apparent_wind], force_labels=["e_x", "wind", "kite_vel", "apparent wind", "true_apparent_wind"])
 
 plt.show()
 
